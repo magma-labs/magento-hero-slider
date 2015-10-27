3973c3bf4550ea660589f102c717c4864188418e
Tadeo Barranco <tbarrancoa@gmail.com>
2015-10-26 22:05:56 -0600

    slider advance

---
+++ app/code/community/Magma/Slider/sql/magma_slider_setup/install-0.0.1.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package Magma
+ * @category Magma_Slider
+ * @author Tadeo Barranco <jose.barranco@magmalabs.io>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+/**
+ * $installer is a variable assigned with the pseudo variable $this value
+ * @var [type]
+ */
+$installer = $this;
+
+/**
+ * Start setup step
+ */
+$installer->startSetup();
+
+/**
+ * Looking for existing slider tables, if exist this will drop it at first time that this code run.
+ */
+if($installer->getConnection()->isTableExists($installer->getTable('slider/slider'))) {
+	$installer->getConnection()->dropTable($installer->getTable('slider/slider'));
+}
+
+if($installer->getConnection()->isTableExists($installer->getTable('slider/options'))) {
+	$installer->getConnection()->dropTable($installer->getTable('slider/options'));
+}
+
+if($installer->getConnection()->isTableExists($installer->getTable('slider/gallery'))) {
+	$installer->getConnection()->dropTable($installer->getTable('slider/gallery'));
+}
+
+/**
+ * Ending setup step
+ */
+$installer->endSetup();
