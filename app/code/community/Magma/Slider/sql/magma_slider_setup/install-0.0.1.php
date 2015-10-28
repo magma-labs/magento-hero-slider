fa9e98a2537064ad411c125d30bf96661f0022ec
Tadeo Barranco <tbarrancoa@gmail.com>
2015-10-27 23:22:32 -0600

    complete slider table creation

--- app/code/community/Magma/Slider/sql/magma_slider_setup/install-0.0.1.php
+++ app/code/community/Magma/Slider/sql/magma_slider_setup/install-0.0.1.php
@@ -5,9 +5,9 @@
  * @category Magma_Slider
  * @author Tadeo Barranco <jose.barranco@magmalabs.io>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- */
-
-/**
+ *
+ *
+ * 
  * $installer is a variable assigned with the pseudo variable $this value
  * @var [type]
  */
@@ -44,6 +44,16 @@ $table = $installer->getConnection()
 				'nullable' => false,
 				'primary' => true,
 			), 'Slider id')
+			->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+				'nullable' => false,
+			), 'Slider title')
+			->addColumn('identifier', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+				'nullable' => false,
+			), 'Slider identifier')
+			->addColumn('is_active', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+				'nullable' => false,
+				'default' => '1',	
+			), 'Slider is active')
 			->setComment('Slider table');
 
 
